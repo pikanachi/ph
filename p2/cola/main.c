dacc0ef33a324db49fe960096f7c89b50fe2addc
Nacho Ortega <74309113+pikanachi@users.noreply.github.com>
2021-11-09 12:43:27 +0100

    cola rm

--- p2/cola/main.c
+++
@@ -1,7 +0,0 @@
-#include "planificador/planificador.h"
-
-int main() {
-    //Llamar al planificador
-    planificador_main();
-    return 0;
-}
--- p2/cola/planificador/planificador.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLANIFICADOR_H
-#define PLANIFICADOR_H
-
-#include "../cola/cola.h"
-#include "../gestor_alarmas/gestor_alarmas.h"
-
-void planificador_main(void);
-
-#endif
